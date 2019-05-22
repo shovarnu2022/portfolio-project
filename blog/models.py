d864af1fd8e370967d25993b7077a1f8a70a7747
SHOVARNU1993 <shovidutta@gmail.com>
2019-05-22 17:20:30 +0530

    New addition to the project

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    images = models.ImageField(upload_to='images/')
+
+
+ 
+
+
+
+#Add the blog app to the settings
+
+#Create a migration
+
+#Migrate
+
+#Add to the admin
