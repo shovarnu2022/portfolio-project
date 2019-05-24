8d21527abdb393780b91fc45d909eda49fcca5ed
SHOVARNU1993 <shovidutta@gmail.com>
2019-05-24 16:12:41 +0530

    Blog Post and Polish

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from . import views 
+
+
+urlpatterns = [
+    path('', views.allblogs, name='allblogs'),
+    path('<int:blog_id>/', views.detail, name='detail'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
